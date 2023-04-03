15c1658ac4841ea48779e6c3720ad4f7b949bc33
gregpiac <gregory.pillac@gmail.com>
2023-04-03 23:42:31 +0200

    ngOnInit pour initialisation des donnees dans face-snap.component.ts

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  /**2 facons d'afficher d'afficher des donnees dynamiques :
+   * string interpolation
+   * attribute binding
+  **/
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+
+  // on initialise maintenant les propriétés dans la methode ngOnInit
+  ngOnInit() {
+    this.title = 'Moustache';
+    this.description = 'La mascotte de la maison';
+    this.createdDate = new Date();
+    this.snaps = 6;
+  }
 
 }
